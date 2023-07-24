bdc262da570cbd3721090733836f1bb149f46fc0
haramsong <alakazam1324@gmail.com>
2023-07-24 17:39:11 +0900

    0724_학습

---
+++ vue-cli/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+// = var App = {
+//   template: '<div>app</div>'
+// }
+// 
+// new Vue({
+//   el : '#app',
+//   render : h => h(App),
+//   components: {
+//     'app': App,
+//   }
+// })
